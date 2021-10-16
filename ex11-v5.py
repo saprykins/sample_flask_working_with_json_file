28b597f862f93db0c9ebe55545d6ce41ad4d342e
saprykins <palatatot@gmail.com>
2021-10-16 19:19:11 +0200

    added: more explicite comments and next steps for developement

--- ex11-v5.py
+++ ex11-v5.py
@@ -1,6 +1,20 @@
 # with pagination
 # strange behavior with handmade json files
 
+"""
+next steps:
+
+1/ split in separate parts:
+- controller(handles request, get file name from html-page)
+- view(prints out)
+- model(changes its status, biz logic)
+
+2/ what to do without extention
+
+3/ what is the difference between .json and .html
+
+"""
+
 
 # command line commands
 # pip install flask
@@ -11,6 +25,8 @@
 # export FLASK_APP=ex11-v2
 # export FLASK_ENV=development # not required line
 # flask run
+# or instead of the last line flask run --reload
+# this will reload server after each file change
 
 # you can check in browser
 # http://127.0.0.1:5000/statuses
@@ -30,11 +46,13 @@ app = Flask(__name__)
 
 
 def read_json():
+# puts data from json-file to array
+
     json_file_in_array = []
     
-    # with open(r"C:/_My_Files/_FA/_ETUDES/python/py-code/day_3/2015-01-01-15.json", encoding="utf-8") as json_input:
+    with open(r"C:/_My_Files/_FA/_ETUDES/python/py-code/day_3/2015-01-01-15.json", encoding="utf-8") as json_input:
     # with open(r"/Users/sio/Desktop/python_scripts/day_3/data_sample.json", encoding="utf-8") as json_input:
-    with open(r"C:/_My_Files/_FA/_ETUDES/python/py-code/day_3/data_sample.json", encoding="utf-8") as json_input:
+    # with open(r"C:/_My_Files/_FA/_ETUDES/python/py-code/day_3/data_sample.json", encoding="utf-8") as json_input:
         for feed in json_input:
             line = json.loads(feed)
             # print(line)
@@ -46,12 +64,17 @@ def read_json():
 
 @app.route('/statuses', methods=['GET', 'POST'])
 def liste_of_statuses():
+# prints out to web-page an array 
+
     array = json_file_in_array
     return str(array)    # return str(array[0]['id'])
 
 
 @app.route('/statuses/<ident>.<format>', methods=['GET', 'POST'])
 def find_the_status(ident, format):
+# prints out from array specifit record defined by ident
+# ident is given from URL
+
     array = json_file_in_array
 
     if format == "json":
@@ -66,9 +89,14 @@ def find_the_status(ident, format):
                 # return str(line) # returns sth
                 return json.dumps(line)  # returns sth
 
+
 # pagination
 @app.route('/statuses/p=<int:page>', methods=['GET', 'POST'])
 def pagination(page):
+# prints out the chosen page of records
+# page number is defined in URL
+# where number of records per line is defined by number_by_page
+
     array = json_file_in_array
     number_by_page = 2
         
